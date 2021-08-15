a2f0f2078e02990f065880ff0cdac36a2903b02b
mariya.pirogova <mariya.pirogova@gocream.ru>
2021-08-16 00:28:31 +0300

    update src

---
+++ src/components/Menu.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="menu">
+    <div class="menu-left">
+      <img src="~@/assets/img/logo.png" width="30" height="30">
+    </div>
+    <div class="menu-right">
+      <router-link to="/about">About</router-link>
+      <router-link to="/services">Services</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,34 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+function loadView(view) {
+  return () => import(/* webpackChunkName: "[request]" */ `@/views/${view}`);
+}
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: loadView("Home"),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/about",
+    name: "About",
+    component: loadView("About"),
+  },
+  {
+    path: "/services",
+    name: "Services",
+    component: loadView("Services"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
 export default router
